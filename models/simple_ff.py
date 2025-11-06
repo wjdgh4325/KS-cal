afe1f38cf8105de1d6fece112cd22f6a3fc95125
jeongho park <134389765+wjdgh4325@users.noreply.github.com>
2025-11-06 16:26:31 +0900

    Update simple_ff.py

--- models/simple_ff.py
+++ models/simple_ff.py
@@ -171,27 +171,30 @@ class SyntheticNN(nn.Module):
         #hidden_sizes = [1024, 1024, 1024]
 
         # re-create original paper results for gamma data with cat model
-        if self.dataset == 'support':
-            hidden_sizes = [56, 28, 28]
-
-        elif self.dataset == 'sequence':
-            hidden_sizes = [96, 48, 48]
-        
-        elif self.dataset in ['mimic', 'liver', 'stomach', 'lung', 'breast']:
-            hidden_sizes = [60, 30, 30]
-        
-        elif self.dataset == 'dlbcl':
-            hidden_sizes = [4096, 2048, 2048, 1024, 1024]
-
-        elif self.dataset == 'nacd':
-            hidden_sizes = [51, 51, 51]
-
-        elif self.dataset == 'whas':
+        if self.dataset == 'whas':
             hidden_sizes = [24, 12, 12]
+
+        elif self.dataset == 'metabric':
+            hidden_sizess = [36, 18, 18]
             
         elif self.dataset == 'gbsg':
             hidden_sizes = [28, 14, 14]
-            
+
+        elif self.dataset == 'nacd':
+            hidden_sizes = [51, 26, 26]
+
+        elif self.dataset == 'sequence':
+            hidden_sizes = [96, 48, 48]
+
+        elif self.dataset == 'support':
+            hidden_sizes = [56, 28, 28]
+ 
+        elif self.dataset == 'mimic':
+            hidden_sizes = [112, 56, 56]
+        
+        elif self.dataset in ['liver', 'stomach', 'lung', 'breast']:
+            hidden_sizes = [60, 30, 30]
+           
         else:
             hidden_sizes = [128, 64, 64]
 
@@ -225,168 +228,3 @@ class SyntheticNN(nn.Module):
         model_args = {'D_in': self.D_in}
 
         return model_args
-
-class MIMICNN(nn.Module):
-    def __init__(self, data_dir, dataset, D_in, model_dist, num_cat_bins, dropout_rate, **kwargs):
-        super(MIMICNN, self).__init__()
-        self.kwargs = kwargs
-        self.data_dir = data_dir
-        self.D_in = D_in
-        self.dataset = dataset
-
-        # assert model_dist in ['cat', 'mtlr'], "need to update this class for non-cat"
-        if self.dataset == 'gbsg':
-            hidden_sizes = [28, 14, 14]
-
-        else:
-            hidden_sizes = [128, 64, 64]
-        #hidden_sizes = [2048, 1024, 1024]
-        # self.model = DeepNet(self.D_in, D_out, ff_hidden_sizes, dropout_rate)
-
-        if model_dist == 'cat':
-            self.model = DeepNet(D_in, num_cat_bins, hidden_sizes, dropout_rate)
-
-        elif model_dist == 'mtlr':
-            self.model = nn.Sequential(DeepNet(D_in, num_cat_bins, hidden_sizes, dropout_rate), MTLRNN(num_cat_bins, num_cat_bins))
-
-        elif model_dist == 'lognormal':
-            self.model = LogNormalhelper(D_in, 1, hidden_sizes, dropout_rate, batchnorm=True)
-
-        else:
-            assert False
-
-    def forward(self, src):
-        pred = self.model(src)
-
-        return pred
-
-    def args_dict(self):
-        """
-        Get a dictionary of args that can be used to reconstruct this architecture.
-        To use the returned dict, initialize the model with `ModelName(**model_args)`.
-        """
-        model_args = {'D_in': self.D_in}
-
-        return model_args
-    
-
-class SurvMNISTNN(nn.Module):
-    def __init__(self, data_dir, D_in, model_dist, num_cat_bins, dropout_rate, **kwargs):
-        super(SurvMNISTNN, self).__init__()
-        self.kwargs = kwargs
-        self.data_dir = data_dir
-        self.D_in = D_in
-        self.model_dist = model_dist
-        hidden_sizes = [512, 1024, 1024]
-        last_filters = 256
-
-        assert model_dist in ['cox', 'mtlr', 'lognormal']
-
-        #self.conv = ConvHelper(F1=32,F2=64,F3=128,F4=last_filters,dr=dropout_rate)
-        #DeepNet(last_filters,num_cat_bins,hidden_sizes,dropout_rate)
-
-        if model_dist == 'mtlr':
-            #self.ff = LeNetFF(last_filters, num_cat_bins)
-            self.conv = LeNetConv(dropout_rate)
-            self.ff = MTLRNN(last_filters, num_cat_bins)
-        
-        elif model_dist == 'lognormal':
-            self.conv1 = LeNetConv(dropout_rate)
-            self.conv2 = LeNetConv(dropout_rate)
-            self.ff = LogNormalMNISTHelper(last_filters, 1, hidden_sizes, dropout_rate)
-
-        elif model_dist == 'cox':
-            self.conv = LeNetConv(dropout_rate)
-            self.ff = CoxNN(last_filters, 1, hidden_sizes, dropout_rate)
-
-        else:
-            assert False
-
-    def forward(self, src):
-        if self.model_dist in ['cox', 'mtlr']:
-            x = self.conv(src) # conv flattens
-            pred = self.ff(x)
-
-        elif self.model_dist == 'lognormal':
-            x1 = self.conv1(src)
-            x2 = self.conv2(src)
-            pred = self.ff(x1, x2)
-
-        return pred
-
-    def args_dict(self):
-        """Get a dictionary of args that can be used to reconstruct this architecture.
-        To use the returned dict, initialize the model with `ModelName(**model_args)`.
-        """
-        model_args = {
-            'D_in': self.D_in,
-        }
-
-        return model_args
-    
-class LeNetConv(nn.Module):
-    def __init__(self, dropout_rate):
-        super(LeNetConv, self).__init__()
-
-        CH1=1
-        CH2=64
-        CH3=128
-        CH4=256
-
-        self.conv1 = nn.Conv2d(in_channels=CH1, out_channels=CH2, kernel_size=5, stride=1)
-        self.conv2 = nn.Conv2d(in_channels=CH2, out_channels=CH3, kernel_size=5, stride=1)
-        self.conv3 = nn.Conv2d(in_channels=CH3, out_channels=CH4, kernel_size=4, stride=1)
-        self.pool1 = nn.MaxPool2d(kernel_size=2)
-        self.pool2 = nn.MaxPool2d(kernel_size=2)
-
-        self.drop1 = nn.Dropout2d(dropout_rate)
-        self.drop2 = nn.Dropout2d(dropout_rate)
-
-    def forward(self, x, _print=False):
-        x = self.conv1(x).relu()
-        
-        if _print:
-            print("x.size()", x.size())
-
-        x = self.drop1(x)
-
-        x = self.pool1(x)
-        
-        if _print:
-            print("x.size()", x.size())
-        
-        x = self.conv2(x).relu()
-        
-        if _print:
-            print("x.size()", x.size())
-
-        x = self.drop2(x)
-
-        x = self.pool2(x)
-        
-        if _print:
-            print("x.size()", x.size())
-        
-        x = self.conv3(x).relu()
-        
-        if _print:
-            print("x.size()", x.size())
-        
-        x = torch.flatten(x, 1)
-        
-        if _print:
-            print("x.size()", x.size())
-
-        return x
-
-class LogNormalMNISTHelper(nn.Module):
-    def __init__(self, D_in, D_out, hidden_sizes, dropout_rate):
-        super(LogNormalMNISTHelper,self).__init__()
-        self.mu_model = DeepNet(D_in, D_out, hidden_sizes, dropout_rate)
-        self.sigma_model = DeepNet(D_in, D_out, hidden_sizes, dropout_rate)
-
-    def forward(self, src1, src2):
-        mu = self.mu_model(src1).view(-1,1)
-        pre_log_sigma = self.sigma_model(src2).view(-1,1)
-        pred = torch.cat([mu, pre_log_sigma],dim=1)
-        return pred
