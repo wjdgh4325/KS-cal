f87e176fc40dab54160a670ec2e5e51e6d1dd3a3
jeongho park <134389765+wjdgh4325@users.noreply.github.com>
2025-11-19 21:07:15 +0900

    Update brier_score.py

--- util/brier_score.py
+++ util/brier_score.py
@@ -18,7 +18,7 @@ def integrated_brier_score(train_tte, train_event, test_tte, test_event, cdf_tes
         test_event_b = test_event[i:i+batch_size]
         surv_b = surv_test[i:i+batch_size]
 
-        ipcw1 = torch.tensor(G.survival_function_at_times((test_tte_b - 1e-8).cpu().numpy()).values, device=DEVICE).view(-1, 1)
+        ipcw1 = torch.tensor(G.survival_function_at_times(test_tte_b.cpu().numpy()).values, device=DEVICE).view(-1, 1)
         ipcw1[ipcw1 == 0] = ipcw1[ipcw1 != 0].min()
 
         uncensored_indicator = (test_tte_b.unsqueeze(1) <= time) & (test_event_b.unsqueeze(1) == 1)
